f9751bc951c7485501478f11dc7a8dcd344a9553
vmpfc1 <vmpfc1@gmail.com>
2016-02-13 04:40:51 -0800

    changed to carousel, js nor working on IE

---
+++ js/image-placeholder.js
@@ -0,0 +1,12 @@
+
+
+
+
+	$('#carousel-example-generic > .carousel-inner > .item > div[rel]').each(function() {
+	  var rel = $(this).attr('rel');
+	  var self = $(this);
+	  var img = new Image();
+	  $(img).load(rel, '', function() {
+	    self.css('background-image', 'url('+rel+')');
+	  });
+	});
