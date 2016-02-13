90f9e0485148a8ee2d3ca09288119f377a0ea1e9
vmpfc1 <vmpfc1@gmail.com>
2016-02-13 09:11:32 -0800

    gifs

--- js/image-placeholder.js
+++ js/image-placeholder.js
@@ -1,6 +1,29 @@
 
 
 
+
+
+
+$('#carousel-example-generic > .carousel-inner > .item > div[rel]').each(function() {
+    var rel  = $(this).attr('rel');
+    var self = $(this);
+    var img  = new Image();
+
+    img.onload = function() {
+        self.css('background-image', 'url(' + rel + ')');
+    }
+
+    if (img.complete) img.onload();
+});
+
+
+
+
+
+
+
+
+/*
 $(window).bind("load", function() {
    
 $('#carousel-example-generic > .carousel-inner > .item > div[rel]').each(function() {
@@ -12,6 +35,6 @@ $('#carousel-example-generic > .carousel-inner > .item > div[rel]').each(functio
 	  });
 	});
 
-   
 });
 	
+*/
